52ad8a1d7d78f544179b767156846047ccaab40f
Beleika <sylvestreberlin@gmail.com>
2024-06-18 22:15:13 -0400

    Week09

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 
-/* Testimony */
+/* Mission */
 var acc = document.getElementsByClassName
     ("accordion");
     var i;
@@ -24,13 +24,33 @@ var acc = document.getElementsByClassName
         });
     }
 
+
+/* Testimony*/
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+/*Cleaning second Part*/
+
+
     /* HAMBURGER BUTTON */
 
-function toggleMenu(){ 
+/*function toggleMenu(){ 
     document.getElementById("navigation").classList.toggle("open"); 
 	document.getElementById("hamButton").classList.toggle("open"); 
   
 } 
 
 const X = document.getElementById("hamButton")
-X.onclick = toggleMenu;
+X.onclick = toggleMenu;*/
